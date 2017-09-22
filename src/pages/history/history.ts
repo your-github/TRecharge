deec57ca05e679943a0567b26966e4a59e195e03
Yourthor Yiaveu <yourthor.yiaveu@gmail.com>
2017-09-22 10:29:42 +0700

    Add call number function to recharge card and show data on history log

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -8,17 +8,31 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class HistoryPage {
 
+  cards = [];
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController
     ) {
+    if(localStorage.getItem("historyCache")){
+      this.cards = JSON.parse(localStorage.getItem("historyCache"));
+      this.cards = this.cards.reverse();
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HistoryPage');
   }
+
   closeHistoryPage(){
     this.viewCtrl.dismiss();
   }
+
+  deleteHistory(index: number){
+    this.cards.splice(index, 1);
+    let newStoreCard = this.cards.slice(0);
+    newStoreCard = newStoreCard.reverse();
+    localStorage.setItem("historyCache", JSON.stringify(newStoreCard))
+  }
+
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, PopoverController, ModalController } from 'ionic-angular';
+import {CallNumber} from '@ionic-native/call-number';
 import {MoreComponent} from '../../components/more/more';
 import {HistoryPage} from '../history/history';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [CallNumber]
 })
 export class HomePage {
-data: string;
+  data: string;
+  inputCardNumber: string = "";
   constructor(
     public navCtrl: NavController,
     public popoverCtrl: PopoverController,
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    public call: CallNumber
   ) {
 
   }
@@ -28,5 +32,24 @@ data: string;
         historyModal.present();
       }
     });
+  }
+  rechargeCard(cnumber: string){
+    if(cnumber.length > 0){
+      const cardNumber = "*121*" + cnumber + "#";
+      const date = new Date();
+      const newCard = {cn: cnumber, d: date.getDate() + '-' + (Number.parseInt(date.getMonth().toString()) + 1) + '-' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes()};
+
+      this.call.callNumber(cardNumber, true).then(()=>{
+        if(localStorage.getItem("historyCache")) {
+          let histories = [];
+          histories = JSON.parse(localStorage.getItem("historyCache"));
+          histories.push(newCard);
+          localStorage.setItem("historyCache", JSON.stringify(histories));
+        }else {
+          localStorage.setItem("historyCache", JSON.stringify([newCard]));
+        }
+        this.inputCardNumber = '';
+      });
+    }
   }
 }
